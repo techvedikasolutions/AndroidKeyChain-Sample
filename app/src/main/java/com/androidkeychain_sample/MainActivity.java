dc28bf18d0c5f9147a92d0e5b456a5481f30c6b8
techvedikasolutions <durga.ambati@techvedika.com>
2017-08-09 16:45:44 +0530

    Commit code for both marshamallow and pre M versions(encryption,decryption).

--- app/src/main/java/com/androidkeychain_sample/MainActivity.java
+++ app/src/main/java/com/androidkeychain_sample/MainActivity.java
@@ -1,197 +1,108 @@
 package com.androidkeychain_sample;
 
-import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.security.KeyPairGeneratorSpec;
 import android.support.annotation.RequiresApi;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
 
-import com.androidkeychain_sample.R;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.math.BigInteger;
+import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
-import java.security.UnrecoverableEntryException;
 import java.security.cert.CertificateException;
-import java.security.interfaces.RSAPublicKey;
-import java.util.Calendar;
 
-import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
-import javax.crypto.CipherOutputStream;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import javax.security.auth.x500.X500Principal;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "log";
+    Button encrypt, decrypt;
+    TextView tv_txt;
+    String plainText = "zLCxR/Rls7IIDCu/SuDlkqTjencr38YC4NZ4B4p3sFcLZzlO2ooiUUht10xhXZSm";
+    KeyHelper keyHelper;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        encrypt = (Button) findViewById(R.id.btn_encrypt);
+        decrypt = (Button) findViewById(R.id.btn_decrypt);
+        tv_txt = (TextView) findViewById(R.id.txt);
+        try {
+            keyHelper = new KeyHelper(MainActivity.this);
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (NoSuchProviderException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        encrypt.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                getorstoreThePrivateKey();
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                try {
+                    String str = keyHelper.encrypt(MainActivity.this, plainText);
+                    keyHelper.storePWDInSharedPrefs(str, MainActivity.this);
+                    tv_txt.setText(str);
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (NoSuchPaddingException e) {
+                    e.printStackTrace();
+                } catch (NoSuchProviderException e) {
+                    e.printStackTrace();
+                } catch (BadPaddingException e) {
+                    e.printStackTrace();
+                } catch (IllegalBlockSizeException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+
             }
         });
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    private void getorstoreThePrivateKey() {
-        String plainText = "zLCxR/Rls7IIDCu/SuDlkqTjencr38YC4NZ4B4p3sFcLZzlO2ooiUUht10xhXZSm";
-        try {
-            KeyStore keyStore = KeyStore.getInstance("AndroidKeyStore");
-            keyStore.load(null);
-
-            //getting alias string as device unique id
-            String alias = getUniqueDeviceId(this);
-
-            int nBefore = keyStore.size();
-
-            // Create the keys if necessary
-            if (!keyStore.containsAlias(alias)) {
-
-                Calendar notBefore = Calendar.getInstance();
-                Calendar notAfter = Calendar.getInstance();
-                notAfter.add(Calendar.YEAR, 1);
-                KeyPairGeneratorSpec spec = new KeyPairGeneratorSpec.Builder(this)
-                        .setAlias(alias)
-                        .setKeyType("RSA")
-                        .setKeySize(2048)
-                        .setSubject(new X500Principal("CN=test"))
-                        .setSerialNumber(BigInteger.ONE)
-                        .setStartDate(notBefore.getTime())
-                        .setEndDate(notAfter.getTime())
-                        .build();
-                KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA", "AndroidKeyStore");
-                generator.initialize(spec);
-
-                KeyPair keyPair = generator.generateKeyPair();
+        decrypt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    String str = keyHelper.decrypt(MainActivity.this, keyHelper.getPWDFromSharedPrefs(MainActivity.this));
+                    tv_txt.setText(str);
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                } catch (NoSuchPaddingException e) {
+                    e.printStackTrace();
+                } catch (NoSuchProviderException e) {
+                    e.printStackTrace();
+                } catch (BadPaddingException e) {
+                    e.printStackTrace();
+                } catch (IllegalBlockSizeException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
             }
-            int nAfter = keyStore.size();
-            Log.v("log", "Before = " + nBefore + " After = " + nAfter);
+        });
 
-            // Retrieve the keys
-            KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry) keyStore.getEntry(alias, null);
-            RSAPublicKey publicKey = (RSAPublicKey) privateKeyEntry.getCertificate().getPublicKey();
-            Log.v("log", "public key = " + publicKey.toString());
-
-            // Encrypt the text
-
-            String dataDirectory = getApplicationInfo().dataDir;
-            String filesDirectory = getFilesDir().getAbsolutePath();
-            String encryptedDataFilePath = filesDirectory + File.separator + "secretlocation";
-
-            Log.v(TAG, "plainText = " + plainText);
-            Log.v(TAG, "dataDirectory = " + dataDirectory);
-            Log.v(TAG, "filesDirectory = " + filesDirectory);
-            Log.v(TAG, "encryptedDataFilePath = " + encryptedDataFilePath);
-
-            Cipher inCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-            inCipher.init(Cipher.ENCRYPT_MODE, publicKey);
-
-            Cipher outCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-            outCipher.init(Cipher.DECRYPT_MODE, privateKeyEntry.getPrivateKey());
-
-            CipherOutputStream cipherOutputStream =
-                    new CipherOutputStream(
-                            new FileOutputStream(encryptedDataFilePath), inCipher);
-            cipherOutputStream.write(plainText.getBytes("UTF-8"));
-            cipherOutputStream.close();
-
-            CipherInputStream cipherInputStream =
-                    new CipherInputStream(new FileInputStream(encryptedDataFilePath),
-                            outCipher);
-            byte[] roundTrippedBytes = new byte[1000]; // TODO: dynamically resize as we get more data
-
-            int index = 0;
-            int nextByte;
-            while ((nextByte = cipherInputStream.read()) != -1) {
-                roundTrippedBytes[index] = (byte) nextByte;
-                index++;
-            }
-            String roundTrippedString = new String(roundTrippedBytes, 0, index, "UTF-8");
-            Log.v(TAG, "round tripped string = " + roundTrippedString);
-
-        } catch (NoSuchAlgorithmException e) {
-
-            Log.e(TAG, Log.getStackTraceString(e));
-        } catch (NoSuchProviderException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-        } catch (InvalidAlgorithmParameterException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-        } catch (KeyStoreException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-        } catch (CertificateException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-        } catch (IOException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-        } catch (UnrecoverableEntryException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-        } catch (NoSuchPaddingException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-        } catch (InvalidKeyException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-        } catch (UnsupportedOperationException e) {
-            Log.e(TAG, Log.getStackTraceString(e));
-        }
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public String getUniqueDeviceId(Context ctx) {
-        String android_id = Settings.Secure.getString(ctx.getContentResolver(),
-                Settings.Secure.ANDROID_ID);
-        return android_id;
-    }
 }
